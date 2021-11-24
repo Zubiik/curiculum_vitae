8ab343a3b8a634efd9bc02b91b57bad6aa21d290
Zubiik <elodiefrappat08@gmail.com>
2021-11-24 13:46:48 +0100

    feature: first styled

---
+++ src/handler/HomePage/Index.js
@@ -0,0 +1,15 @@
+import ScrollButton from "../component/Button/Index";
+import {Container} from "./styled";
+
+function HomePage() {
+
+
+    return (
+      <Container>
+        <ScrollButton/>
+      </Container>
+    );
+  }
+  
+  export default HomePage;
+  
