778a6cdf28cd14cc2f59ddba5fcff2f85a9cffb9
Zubiik <elodiefrappat08@gmail.com>
2021-12-10 19:13:19 +0100

    feat: component box

---
+++ src/component/Box/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container } from "./styled";
+
+function Box() {
+  return (
+    <Container>
+      <p>
+        Where does it come from? Contrary to popular belief, Lorem Ipsum is not
+        simply random text. It has roots in a piece of classical Latin
+        literature from 45 BC, making it over 2000 years old. Richard
+        McClintock, a Latin professor at
+      </p>
+    </Container>
+  );
+}
+
+export default Box;
---
+++ src/component/Box/styled.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  background-color: #1B2932;
+  color: white;
+  min-width: 250px;
+  min-height: 200px;
+  margin: 10px;
+  padding:20px;
+  border-radius: 40px;
+`;
