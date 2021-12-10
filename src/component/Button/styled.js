2dc601438e6a9ae0470cb72b3118f35fc6c4ecbe
Zubiik <elodiefrappat08@gmail.com>
2021-12-10 19:14:30 +0100

    bazar

--- src/component/Button/styled.js
+++ src/component/Button/styled.js
@@ -2,18 +2,18 @@ import styled from "@emotion/styled";
 
 export const CustomButton = styled.button`
   border-radius: 40px;
-  background-color: black;
-  border: #dcb253 4px solid;
+  background-color: #1B2932;
+  border: #3F8D6C 4px solid;
   max-height: 50px;
   min-width: 100px;
   margin: 4px;
-  color: #dcb253;
+  color: #3F8D6C;
   & {
     transition-duration: 0.4s;
   }
 
   &:hover {
-    color: black;
-    background-color: #dcb253;
+    color: #1B2932;
+    background-color: #3F8D6C;
   }
 `;
