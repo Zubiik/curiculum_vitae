f4302574db459c534c9386a0efc79a3737585973
Zubiik <elodiefrappat08@gmail.com>
2021-12-13 16:35:53 +0100

    fix: organisation

--- src/Studies/index.js
+++ src/Studies/index.js
@@ -1,25 +1,32 @@
 import React from "react";
 import Box from "../component/Box";
-import { BoxContainer, ParcoursContainer } from "./styled";
+import { ImageContainer, ParcoursContainer } from "./styled";
 import Title from "../component/Title";
 
 function Studies() {
   return (
-    <ParcoursContainer>
+    <div>
       <Title>
         <p>PARCOURS</p>
       </Title>
-      <BoxContainer>
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-      </BoxContainer>
-    </ParcoursContainer>
+      <ParcoursContainer>
+        <Box>
+          <p>
+            Where does it come from? Contrary to popular belief, Lorem Ipsum is
+            not simply random text. It has roots in a piece of classical Latin
+            literature from 45 BC, making it over 2000 years old. Richard
+            McClintock, a Latin professor at Where does it come from? Contrary
+            to popular belief, Lorem Ipsum is not simply random text. It has
+            roots in a piece of classical Latin literature from 45 BC, making it
+            over 2000 years old. Richard McClintock, a Latin professor at Where
+            does it come from? Contrary to popular belief, Lorem Ipsum is not
+            simply random text. It has roots in a piece of classical Latin
+            literature from 45 BC, making it over 2000 years old. Richard
+            McClintock, a Latin professor at
+          </p>
+        </Box>
+      </ParcoursContainer>
+    </div>
   );
 }
 
