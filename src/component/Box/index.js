30d6b56f26e9aab378726811a13eefe60b61d9c3
Zubiik <elodiefrappat08@gmail.com>
2021-12-13 16:36:30 +0100

    fix:organisation

--- src/component/Box/index.js
+++ src/component/Box/index.js
@@ -1,17 +1,8 @@
 import React from "react";
 import { Container } from "./styled";
 
-function Box() {
-  return (
-    <Container>
-      <p>
-        Where does it come from? Contrary to popular belief, Lorem Ipsum is not
-        simply random text. It has roots in a piece of classical Latin
-        literature from 45 BC, making it over 2000 years old. Richard
-        McClintock, a Latin professor at
-      </p>
-    </Container>
-  );
+function Box({ className, children}) {
+  return <Container className={className}>{children}</Container>;
 }
 
 export default Box;
