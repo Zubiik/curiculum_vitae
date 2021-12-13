3d2df037c895c726f4bc4cdb5a2ec3bca086b8a5
Zubiik <elodiefrappat08@gmail.com>
2021-12-13 14:46:54 +0100

    fix:useless styled component

--- src/Studies/styled.js
+++ src/Studies/styled.js
@@ -1,6 +1,5 @@
 import styled from "@emotion/styled";
 
-export const Container = styled.div``;
 export const BoxContainer = styled.div`
   width: 80%;
   display: flex;
@@ -9,10 +8,13 @@ export const BoxContainer = styled.div`
   }
   overflow-x: scroll;
   border-radius: 20px;
-  box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgb(209, 213, 219) 0px 0px 0px 1px inset;
+  background: #c4c4c447;
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px,
+    rgb(209, 213, 219) 0px 0px 0px 1px inset;
 `;
 
 export const ParcoursContainer = styled.div`
+  height: 400px;
   display: flex;
   flex-direction: column;
   width: 100%;
