94d14924db4390bfd0caf95f16fb6d82d6049092
Zubiik <elodiefrappat08@gmail.com>
2021-12-14 11:17:34 +0100

    feat: colors of theme

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,16 @@
 export const theme = {
-
+colors:{
+    copperRed: '#C37455',
+    gunMetal:'#1B2932',
+    slateGray:'#5E7887',
+    tan:'#D3B692',
+    puce:'#CC8899',
+    emerald:'#3F8D6C',
+   
+},
+lightColors: {
+    cameoPink:'#DFB8C2',
+    beauBlue:'#B6C7D0',
+    opal:'#B0C8BE',
+},
   };
