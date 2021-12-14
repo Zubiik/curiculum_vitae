09c7a6d704f9394b9b52d67b50ceae1d5b43c9a0
Zubiik <elodiefrappat08@gmail.com>
2021-12-14 11:43:38 +0100

    fix: studies style

--- src/Studies/index.js
+++ src/Studies/index.js
@@ -1,32 +1,30 @@
 import React from "react";
 import Box from "../component/Box";
-import { ImageContainer, ParcoursContainer } from "./styled";
-import Title from "../component/Title";
+import {  Container, ParcoursContainer } from "./styled";
+import figmaImage from "../images/ex.png";
 
 function Studies() {
   return (
-    <div>
-      <Title>
-        <p>PARCOURS</p>
-      </Title>
+    <Container>
       <ParcoursContainer>
-        <Box>
-          <p>
-            Where does it come from? Contrary to popular belief, Lorem Ipsum is
-            not simply random text. It has roots in a piece of classical Latin
-            literature from 45 BC, making it over 2000 years old. Richard
-            McClintock, a Latin professor at Where does it come from? Contrary
-            to popular belief, Lorem Ipsum is not simply random text. It has
-            roots in a piece of classical Latin literature from 45 BC, making it
-            over 2000 years old. Richard McClintock, a Latin professor at Where
-            does it come from? Contrary to popular belief, Lorem Ipsum is not
-            simply random text. It has roots in a piece of classical Latin
-            literature from 45 BC, making it over 2000 years old. Richard
-            McClintock, a Latin professor at
-          </p>
-        </Box>
+          <img src={figmaImage} alt="i" />
+          <Box>
+            <p>
+              Where does it come from? Contrary to popular belief, Lorem Ipsum
+              is not simply random text. It has roots in a piece of classical
+              Latin literature from 45 BC, making it over 2000 years old.
+              Richard McClintock, a Latin professor at Where does it come from?
+              Contrary to popular belief, Lorem Ipsum is not simply random text.
+              It has roots in a piece of classical Latin literature from 45 BC,
+              making it over 2000 years old. Richard McClintock, a Latin
+              professor at Where does it come from? Contrary to popular belief,
+              Lorem Ipsum is not simply random text. It has roots in a piece of
+              classical Latin literature from 45 BC, making it over 2000 years
+              old. Richard McClintock, a Latin professor at
+            </p>
+          </Box>
       </ParcoursContainer>
-    </div>
+    </Container>
   );
 }
 
--- src/Studies/styled.js
+++ src/Studies/styled.js
@@ -1,14 +1,16 @@
 import styled from "@emotion/styled";
 
-
-
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: ${({ theme }) => theme.lightColors.isabeline};
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 15px 10px -15px;
+`;
 export const ParcoursContainer = styled.div`
-  height: 600px;
+  width: 70%;
   display: flex;
   justify-content: center;
   align-items: center;
+  
 `;
-export const ImageContainer = styled.img`
-
-`;
-
+export const ImageContainer = styled.img``;
