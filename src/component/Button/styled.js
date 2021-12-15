e3df4acf772fe8ad8df0912abcb89fc5f3abea32
Zubiik <elodiefrappat08@gmail.com>
2021-12-15 20:53:03 +0100

    fix: theme color

--- src/component/Button/styled.js
+++ src/component/Button/styled.js
@@ -3,17 +3,17 @@ import styled from "@emotion/styled";
 export const CustomButton = styled.button`
   border-radius: 40px;
   background-color: ${({ theme }) => theme.colors.gunMetal};
-  border: ${({theme}) => theme.colors.emerald} 4px solid;
+  border: ${({ theme }) => theme.colors.emerald} 4px solid;
   max-height: 50px;
   min-width: 100px;
   margin: 4px;
-  color: ${({theme}) => theme.colors.emerald};
+  color: ${({ theme }) => theme.colors.emerald};
   & {
     transition-duration: 0.4s;
   }
-
+  font-weight: bold;
   &:hover {
     color: ${({ theme }) => theme.colors.gunMetal};
-    background-color: ${({theme}) => theme.colors.emerald};
+    background-color: ${({ theme }) => theme.colors.emerald};
   }
 `;
