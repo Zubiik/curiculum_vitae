ac495417284be7049131d55e20136043d8e1fb35
Zubiik <elodiefrappat08@gmail.com>
2021-12-15 20:52:08 +0100

    fix: width and height

--- src/component/Box/styled.js
+++ src/component/Box/styled.js
@@ -2,8 +2,6 @@ import styled from "@emotion/styled";
 
 export const Container = styled.div`
   box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
-  min-width: 150px;
-  min-height: 100px;
   margin: 10px;
   padding: 20px;
   border-radius: 20px;
