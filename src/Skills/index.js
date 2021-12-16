bff767ba299657b20d2d05b17ad5f3481f0b30f3
Zubiik <elodiefrappat08@gmail.com>
2021-12-16 12:40:20 +0100

    fix: skills stylisation

--- src/Skills/index.js
+++ src/Skills/index.js
@@ -1,66 +1,84 @@
 import React from "react";
 import {
-  Container,
+  BoxContainer,
   TitleCustom,
+  CustomTextLine,
   StarsContainer,
   GreenTitleCustom,
   BoldText,
+  TextsContainer,
+  Container,
+  ImageCustom,
+  ImageAndBoxCustom,
+  ImageAndBoxCustomLeft
 } from "./styled";
 import PinkStars from "../component/PinkStars";
 import GreenStars from "../component/GreenStars";
+import frontEndImg from "../images/front-end.png";
+import design from "../images/design-icon.png";
+import workflow from "../images/workflow.png";
 
 function Skills() {
   return (
-    <div>
-      <Container>
-        <TitleCustom>Front-End</TitleCustom>
-        <StarsContainer>
-          <p>React</p>
-          <PinkStars level={[1, 2, 3, 4, 5]} />
-        </StarsContainer>
-        <StarsContainer>
-          <p>Javascript</p>
-          <PinkStars level={[1, 2, 3, null, null]} />
-        </StarsContainer>
-        <StarsContainer>
-          <p>React native</p>
-          <PinkStars level={[1, 2, 3, null, null]} />
-        </StarsContainer>
-      </Container>
+    <Container>
+      <ImageAndBoxCustom>
+        <BoxContainer>
+          <TitleCustom>Front-End</TitleCustom>
+          <StarsContainer>
+            <p>React</p>
+            <PinkStars level={[1, 2, 3, 4, 5]} />
+          </StarsContainer>
+          <StarsContainer>
+            <p>Javascript</p>
+            <PinkStars level={[1, 2, 3, null, null]} />
+          </StarsContainer>
+          <StarsContainer>
+            <p>React native</p>
+            <PinkStars level={[1, 2, 3, null, null]} />
+          </StarsContainer>
+        </BoxContainer>
+          <ImageCustom src={frontEndImg} alt="frontEnd icon" />
+      </ImageAndBoxCustom>
 
-      <Container>
-        <GreenTitleCustom>Design</GreenTitleCustom>
-        <StarsContainer>
-          <p>Figma</p>
-          <GreenStars level={[1, 2, 3, null, null]} />
-        </StarsContainer>
-        <StarsContainer>
-          <p>Maquettage</p>
-          <GreenStars level={[1, 2, null, null, null]} />
-        </StarsContainer>
-        <StarsContainer>
-          <p>Prototypage</p>
-          <GreenStars level={[1, null, null, null, null]} />
-        </StarsContainer>
-      </Container>
-      <Container>
-        <GreenTitleCustom>Environnement de travail</GreenTitleCustom>
-        <StarsContainer>
-          <span>
-            <BoldText>système d'exploitation: </BoldText>Linux
-          </span>
-        </StarsContainer>
-        <StarsContainer>
-          <span>
-            <BoldText>Workflow:</BoldText> Vs Code,Postman,Git, yarn
-          </span>
-        </StarsContainer>
-        <StarsContainer>
-            <BoldText>IDE: </BoldText> <span>Visual Studio Code
-          </span>
-        </StarsContainer>
-      </Container>
-    </div>
+      <ImageAndBoxCustomLeft>
+        <BoxContainer>
+          <GreenTitleCustom>Design</GreenTitleCustom>
+          <StarsContainer>
+            <p>Figma</p>
+            <GreenStars level={[1, 2, 3, null, null]} />
+          </StarsContainer>
+          <StarsContainer>
+            <p>Maquettage</p>
+            <GreenStars level={[1, 2, null, null, null]} />
+          </StarsContainer>
+          <StarsContainer>
+            <p>Prototypage</p>
+            <GreenStars level={[1, null, null, null, null]} />
+          </StarsContainer>
+        </BoxContainer>
+        <ImageCustom src={design} alt="design icon" />
+      </ImageAndBoxCustomLeft>
+
+      <ImageAndBoxCustom>
+        <BoxContainer>
+          <GreenTitleCustom>Environnement de travail</GreenTitleCustom>
+          <TextsContainer>
+            <CustomTextLine>
+              <BoldText>système d'exploitation: </BoldText>Linux
+            </CustomTextLine>
+
+            <CustomTextLine>
+              <BoldText>Workflow:</BoldText> Vs Code,Postman,Git, yarn
+            </CustomTextLine>
+
+            <CustomTextLine>
+              <BoldText>IDE: </BoldText> Visual Studio Code
+            </CustomTextLine>
+          </TextsContainer>
+        </BoxContainer>
+        <ImageCustom src={workflow} alt="workflow icon" />
+      </ImageAndBoxCustom>
+    </Container>
   );
 }
 
--- src/Skills/styled.js
+++ src/Skills/styled.js
@@ -1,20 +1,22 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
+  width: 100%;
+  background-color: #F2F2F2 ;
+`;
+export const BoxContainer = styled.div`
+  width: 30%;
   display: flex;
   flex-direction: column;
   box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
   padding: 20px;
   margin: 10px;
-  width: 300px;
   border-radius: 20px;
   background: ${({ theme }) => theme.lightColors.lightWhite};
 `;
 
 export const StarsContainer = styled.div`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
 `;
 
 export const TitleCustom = styled.h3`
@@ -23,6 +25,38 @@ export const TitleCustom = styled.h3`
 export const GreenTitleCustom = styled.h3`
   color: ${({ theme }) => theme.colors.emerald};
 `;
-export const BoldText = styled.p`
+export const BoldText = styled.span`
+  margin-bottom: 10px;
   font-weight: bold;
+`;
+export const TextsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const CustomTextLine = styled.div`
+  margin-bottom: 10px;
+`;
+
+export const ImageCustom = styled.img`
+  height: 150px;
+  &{
+     animation: bounce 0.7s ease infinite;
+}
+
+@keyframes bounce{
+     from {top: 106px;}
+     50%  {top: 86px;}
+     to   {top: 106px;}
+}
+`;
+
+export const ImageAndBoxCustom = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const ImageAndBoxCustomLeft = styled(ImageAndBoxCustom)`
+  display: flex;
+  flex-direction: row-reverse;
 `;
