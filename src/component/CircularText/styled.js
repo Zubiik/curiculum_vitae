e99310af20ea8144ad0f02fb2180590f35e4ea84
Zubiik <elodiefrappat08@gmail.com>
2021-12-21 10:49:06 +0100

    delete: useless component

--- src/component/CircularText/styled.js
+++
@@ -1,9 +0,0 @@
-export const CircularText = styled.span`
-  transform: rotate(8.1deg);
-  color: blue;
-  width: 100%;
-  height: 100%;
-  letter-spacing: 2rem;
-  position: absolute;
-  transform-origin: 0 180px;
-`;
