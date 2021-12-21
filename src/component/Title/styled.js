a60a912b94afefe9dfe696c4495343486d33f413
Zubiik <elodiefrappat08@gmail.com>
2021-12-21 19:58:13 +0100

    fix: padding

--- src/component/Title/styled.js
+++ src/component/Title/styled.js
@@ -3,7 +3,6 @@ import styled from "@emotion/styled";
 export const CustomTitle = styled.h1`
   display: flex;
   color: ${({ theme }) => theme.colors.copperRed};
-  margin: 20px;
   align-items: center;
   text-align: center;
   height: 200px;
