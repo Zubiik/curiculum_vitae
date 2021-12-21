3b22e4a1fa6908a6999f71a6b90d0678aeff05e0
Zubiik <elodiefrappat08@gmail.com>
2021-12-21 19:59:00 +0100

    fix: styled changes

--- src/Studies/styled.js
+++ src/Studies/styled.js
@@ -1,22 +1,20 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
+  padding: 40px;
   display: flex;
-  flex-direction: column;
-  background: ${({ theme }) => theme.lightColors.isabeline};
+  justify-content: space-around;
+  background-color: ${({ theme }) => theme.lightColors.isabeline};
   box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
 `;
 export const ParcoursContainer = styled.div`
-  width: 70%;
+  width: 100%;
   display: flex;
-  justify-content: center;
-  align-items: center;
   @media all and (max-width: 480px) {
     width: 100%;
   }
 `;
 export const ImageCustom = styled.img`
-  margin-left: 40px;
   margin-right: 40px;
   @media all and (max-width: 480px) {
     display: none;
